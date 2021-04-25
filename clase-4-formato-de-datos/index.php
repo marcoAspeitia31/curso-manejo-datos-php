8afd024f53e1d8e7e139ff1b1f9d8977d8a12bb7
Marco Aspeitia <maspeitia.31@gmail.com>
2021-04-25 16:50:42 -0500

    clase 4, reemplazar y modificar

--- clase-4-formato-de-datos/index.php
+++ clase-4-formato-de-datos/index.php
@@ -17,4 +17,31 @@ echo '<br>';
 
 echo ucfirst($text);
 
+echo '<br>';
+
 //Reemplazar
+$slug = str_replace(' ', '-', $text);
+
+echo strtolower($slug);
+
+echo '<br>';
+
+//Modificacion
+$code = 546;
+
+echo str_pad($code, 10, '#', STR_PAD_LEFT);
+echo '<br>';
+echo str_pad($code, 10, '#', STR_PAD_RIGHT);
+echo '<br>';
+echo str_pad($code, 10, '#', STR_PAD_BOTH);
+echo '<br>';
+
+$html_text = '<h1>hola mamá</h1>';
+
+echo strip_tags($html_text); //remueve etiquetas html de mi variable $html_text
+echo '<br>';
+
+echo strtoupper(strip_tags($html_text)); //monobyte
+echo '<br>';
+
+echo mb_strtoupper(strip_tags($html_text)); //multibyte (ayuda a formatear caractéres extraños como la ñ o acentos)
