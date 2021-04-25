c32ca2c499181a2db63a23159153cab6a9f9a65f
Marco Aspeitia <maspeitia.31@gmail.com>
2021-04-25 16:34:59 -0500

    clase 4 alterar strings

---
+++ clase-4-formato-de-datos/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// Alterar
+$text = "PHP es UN LENGUAJE de programacion";
+
+echo strtolower($text);
+
+echo '<br>';
+
+echo strtoupper($text);
+
+echo '<br>';
+
+echo lcfirst($text);
+
+echo '<br>';
+
+echo ucfirst($text);
+
+//Reemplazar
